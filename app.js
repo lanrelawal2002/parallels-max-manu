6accb153b84272b35d911901bf4ace63cb97c013
lanrelawal0022 <lanrelawal2002@gmail.com>
2025-12-02 14:35:40 +0100

    added views and app setting

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  res.send("<p>You reached the landing page.</p>");
+  res.render("index");
 });
 
-app.get("/current-time", function (req, res) {
-  res.send("<p>" + new Date().toISOString() + "</p>");
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
+});
+
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+app.get("/restaurants", function (req, res) {
+  res.render("restaurants");
 });
 
 app.listen(3000);
