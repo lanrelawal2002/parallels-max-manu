858e885f172edba0b728c202d492cbf1065229f1
lanrelawal0022 <lanrelawal2002@gmail.com>
2025-12-03 16:23:07 +0100

    more ejs usage

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const fs = require("fs");
+
 const express = require("express");
 
 const app = express();
@@ -7,6 +9,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
@@ -25,8 +28,27 @@ app.get("/recommend", function (req, res) {
   res.render("recommend");
 });
 
+app.post("/recommend", function (req, res) {
+  const restaurantData = req.body;
+  const databasePath = path.join(__dirname, "database", "restaurants.json");
+  const databaseContent = fs.readFileSync(databasePath);
+  const restaurantsArray = JSON.parse(databaseContent);
+
+  restaurantsArray.push(restaurantData); // restaurantData is a JS object
+  fs.writeFileSync(databasePath, JSON.stringify(restaurantsArray));
+
+  res.redirect("/confirm");
+});
+
 app.get("/restaurants", function (req, res) {
-  res.render("restaurants");
+  const databasePath = path.join(__dirname, "database", "restaurants.json");
+  const databaseContent = fs.readFileSync(databasePath);
+  const restaurantsArray = JSON.parse(databaseContent);
+
+  res.render("restaurants", {
+    numberOfRestaurants: restaurantsArray.length,
+    restaurants: restaurantsArray,
+  });
 });
 
 app.listen(3000);
